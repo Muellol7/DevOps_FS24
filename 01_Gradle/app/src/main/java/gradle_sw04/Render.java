aba7a9a718f18ee8a67acc1453f118f4a0124192
Muellol7 <muellol7@students.zhaw.ch>
2024-05-20 16:56:52 +0200

    Quick Fix Ascii 02

---
+++ 01_Gradle/app/src/main/java/gradle_sw04/Render.java
@@ -0,0 +1,24 @@
+package gradle_sw04;
+
+import com.indvd00m.ascii.render.api.ICanvas;
+import com.indvd00m.ascii.render.api.IContext;
+import com.indvd00m.ascii.render.api.IContextBuilder;
+
+public class Render {
+
+    public void setPseudoCanvas(boolean b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPseudoCanvas'");
+    }
+
+    public IContextBuilder newBuilder() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'newBuilder'");
+    }
+
+    public ICanvas render(IContext iContext) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'render'");
+    }
+
+}
